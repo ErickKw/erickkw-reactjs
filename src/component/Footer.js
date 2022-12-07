533b21bf9f564ff95bf05fc6e328b3eb81ec1242
ErickKw <erickkusumaw@gmail.com>
2022-12-08 00:50:56 +0700

    Modified Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,12 +3,9 @@ import "./style.css";
 
 function Footer(prop) {
   return (
-    <footer className="footer-section">
-      <div className="footer-brand">
-        <img src={logo} alt="logo"></img>
-        <p>Copyright 2022 Erick Kusuma Wardani</p>
-      </div>
-    </footer>
+    <div className="footer">
+      <p>Copyright 2022 Erick Kusuma Wardani </p>
+    </div>
   );
 }
 
